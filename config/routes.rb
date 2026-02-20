251daf1be625ff42ee33e267ea72fc5e1595c21c
preno02 <parker.reno@kellogg.northwestern.edu>
2026-02-20 00:37:16 +0000

    Updated Routes file to link to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   # 
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+get("/places", :controller => "places", :action => "index")
+# get("/places/France", :controller => "places", :action => "france")
+# get("/places/Florida", :controller => "places", :action => "florida")
+# get("/places/Colorado", :controller => "places", :action => "colorado")
+# get("/place/Montana", :controller => "places", :action => "montana")
+
+
+
+#   # Defines the root path route ("/")
+#   # get("/", { :controller => "articles", :action => "index" })
+#   # 
+  # resources "places"
+  # resources "entries"
+  
 end
