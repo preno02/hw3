b4dcec3cc7d1615de4a96e993e46dd90bf9dcbf1
preno02 <parker.reno@kellogg.northwestern.edu>
2026-02-20 21:45:49 +0000

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 #   # 
-
-get("/places", :controller => "places", :action => "index")
+get("/", { :controller => "places", :action => "index" })
+get("/places", { :controller => "places", :action => "index" })
 get("/places", :controller => "places", :action => "show")
-
-# get("/places/France", :controller => "france", :action => "france")
-# get("/places/Florida", :controller => "florida", :action => "florida")
-# get("/places/Colorado", :controller => "colorado", :action => "colorado")
-# get("/place/Montana", :controller => "montana", :action => "montana")
-
-
 
 #   # Defines the root path route ("/")
 #   # get("/", { :controller => "articles", :action => "index" })
